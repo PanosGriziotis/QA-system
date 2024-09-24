31f7fd57bec6ea1bd49a2230914d91f0bec0d56a
PanosGriziotis <panosgriz@gmail.com>
2024-09-24 19:47:13 +0300

    cleaned file

--- src/pipelines/indexing_pipeline.py
+++ src/pipelines/indexing_pipeline.py
@@ -21,18 +21,7 @@ embedding_model = "panosgriz/covid_el_paraphrase-multilingual-MiniLM-L12-v2"
 retriever = EmbeddingRetriever(embedding_model=embedding_model, document_store=DOCUMENT_STORE)
 tokenizer = AutoTokenizer.from_pretrained("ilsp/Meltemi-7B-v1")
 
-''''
-preprocessor = PreProcessor(
-    clean_empty_lines=True,
-    clean_header_footer=True,
-    clean_whitespace=True,
-    split_by="token",
-    split_length=128,
-    split_overlap=0,
-    split_respect_sentence_boundary=True
-)
 
-'''
 preprocessor = PreProcessor(
     clean_empty_lines=True,
     clean_whitespace=True,
@@ -44,7 +33,6 @@ preprocessor = PreProcessor(
     tokenizer=tokenizer
     )
 
-#DOCUMENT_STORE.recreate_index = True
 indexing_pipeline = init_file_to_doc_pipeline(custom_preprocessor=preprocessor)
 # Update the document embeddings in the the document store using the encoding model specified in the retriever
 indexing_pipeline.add_node(component=retriever, name = "DenseRetriever", inputs=["Preprocessor"])
